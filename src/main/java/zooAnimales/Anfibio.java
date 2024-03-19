51c9965a36f02c0da4c0c3f3f84e60939ed0808f
JoselitoPalacio <jpalaciose@unal.edu.co>
2024-03-19 00:37:32 -0500

    ,,,,

--- src/main/java/zooAnimales/Anfibio.java
+++ src/main/java/zooAnimales/Anfibio.java
@@ -1,110 +1,66 @@
 package zooAnimales;
-
-import gestion.Zona;
+import java.util.ArrayList;
 
 public class Anfibio extends Animal{
-    
-    private Anfibio[] listado;
-    private String colorPiel;
-    private boolean venenoso;
-    private static int total = 0; //1;
+	private static ArrayList<Anfibio> listado=new ArrayList<>();
+	public static int ranas;
+	public static int salamandras;
+	private String colorPiel;
+	private boolean venenoso;
+	
+	public Anfibio() {
+		this(null, 0, null, null, null, false);
+	}
+	
+	public Anfibio(String nombre, int edad, String habitat, String genero, String colorPiel, boolean venenoso) {
+		super(nombre,edad,habitat,genero);
+		this.colorPiel=colorPiel;
+		this.venenoso=venenoso;
+		Anfibio.listado.add(this);
+	}
+	
+	
+	public static void setListado(ArrayList<Anfibio> listado) {
+		Anfibio.listado=listado;
+	}
+	public static ArrayList<Anfibio> getListado() {
+		return listado;
+	}
+	
+	public void setColorPiel(String colorPiel) {
+		this.colorPiel=colorPiel;
+	}
+	public String getColorPiel() {
+		return colorPiel;
+	}
+	
+	public void setVenenoso(boolean venenoso) {
+		this.venenoso=venenoso;
+	}
+	public boolean isVenenoso() {
+		return venenoso;
+	}
+	
+	public int cantidadAnfibios() {
+		return Anfibio.listado.size();
+		
+	}
+	
+	public String movimiento() {
+		return "saltar";
+	}
+	
+	public static Anfibio crearRana(String nombre, int edad, String genero) {
+		Anfibio Anfibio =new Anfibio(nombre,edad, "selva",genero, "rojo", true);
+		ranas++;
+		return Anfibio;
+	}
+	
+	public static Anfibio crearSalamandra(String nombre, int edad, String genero) {
+		Anfibio Anfibio=new Anfibio(nombre, edad, "selva",genero, "negro y amarillo", false);
+		salamandras++;
+		return Anfibio;
+		
+	}
 
-    static public int ranas ;
-    static public int salamandras;
-
-    public String movimiento = "saltar";
-
-    public Anfibio( String nombre, int edad, String habitat, String genero,
-    
-    String colorPiel,
-    boolean venenoso) {
-
-        super(nombre, edad, habitat, genero);
-        
-        this.colorPiel = colorPiel;
-        this.venenoso = venenoso;
-        total ++;
-
-        
-    }
-    public Anfibio(){
-
-        total ++;
-        
-    }
-
-    public Anfibio[] getListado() {
-        return listado;
-    }
-
-    public void setListado(Anfibio[] listado) {
-        this.listado = listado;
-    }
-
-    public String getColorPiel() {
-        return colorPiel;
-    }
-
-    public void setColorPiel(String colorPiel) {
-        this.colorPiel = colorPiel;
-    }
-
-    public boolean isVenenoso() {
-        return venenoso;
-    }
-
-    public void setVenenoso(boolean venenoso) {
-        this.venenoso = venenoso;
-    }
-
-    public static int getRanas() {
-        return ranas;
-    }
-
-    public static void setRanas(int ranas) {
-        Anfibio.ranas = ranas;
-    }
-
-    public static int getSalamandras() {
-        return salamandras;
-    }
-
-    public static void setSalamandras(int salamandras) {
-        Anfibio.salamandras = salamandras;
-    }
-
-    public static int cantidadAnfibios() {
-        return Anfibio.salamandras + Anfibio.ranas + total;
-    }
-
-    public static Anfibio crearRana(String nombre, int edad, String genero) {
-        Anfibio rana = new Anfibio();
-        rana.colorPiel = "rojo";
-        rana.venenoso = true;
-        rana.habitat = "selva";
-        rana.nombre = nombre;
-        rana.edad = edad;
-        rana.genero = genero;
-        rana.ranas ++;
-        //Animal.totalAnimales ++;
-        return rana;
-    }
-
-    public static Anfibio crearSalamandra(String nombre, int edad, String genero) {
-        Anfibio salamandra = new Anfibio();
-        salamandra.colorPiel = "negro y amarillo";
-        salamandra.venenoso = false;
-        salamandra.habitat = "selva";
-        salamandra.nombre = nombre;
-        salamandra.edad = edad;
-        salamandra.genero = genero;
-        salamandra.salamandras ++;
-        //Animal.totalAnimales ++;
-        return salamandra;
-        
-    }
-
-    public String movimiento(){
-        return movimiento;
-    }
 }
--- src/main/java/zooAnimales/Ave.java
+++ src/main/java/zooAnimales/Ave.java
@@ -1,51 +1,56 @@
 package zooAnimales;
 import java.util.ArrayList;
-import gestion.Zona;
 
-public class Ave extends Animal {
-	private static ArrayList<Ave> listado =new ArrayList<Ave>();
+public class Ave extends Animal{
+	private static ArrayList<Ave> listado = new ArrayList<>();
 	public static int halcones;
 	public static int aguilas;
 	private String colorPlumas;
 	
-	public Ave(String nombre, int edad, String habitat,String genero,String colorPlumas) {
-		super(nombre,edad,habitat,genero);
-		this.colorPlumas=colorPlumas;
-		listado.add(this);
-	}
 	public Ave() {
-		this(null,0,null,null,null);
+		this(null, 0, null, null, null);
+	}
 	
-	}
-	public int cantidadAves() {
-	return listado.size();
-	}
-	public String movimiento(){
-		return "volar";
-	}
-	public static Ave crearHalcon(String nombre,int edad,String genero){
-		Ave Halcon=new Ave(nombre,edad,"montanas",genero,"cafe glorioso");
-		Ave.halcones++;
-		return Halcon;
-	}
-	public static Ave crearAguila(String nombre,int edad,String genero){
-		Ave Aguila=new Ave(nombre,edad,"montanas",genero,"blanco y amarillo");
-		Ave.aguilas++;
-		return Aguila;
-	}
-	public String getColorPlumas() {
-		return colorPlumas;
-	}
-
-	public void setColorPlumas(String colorPlumas) {
+	public Ave(String nombre, int edad, String habitat, String genero, String colorPlumas) {
+		super(nombre, edad, habitat, genero);
 		this.colorPlumas = colorPlumas;
+		Ave.listado.add(this);
+	}
+	
+	public static void setListado(ArrayList<Ave> listado) {
+		Ave.listado = listado;
 	}
 	
 	public static ArrayList<Ave> getListado() {
 		return listado;
 	}
 	
-	public static void setListado(ArrayList<Ave> listado) {
-		Ave.listado = listado;
+	public void setColorPlumas(String colorPlumas) {
+		this.colorPlumas = colorPlumas;
 	}
+	
+	public String getColorPlumas() {
+		return colorPlumas;
+	}
+	
+	public int cantidadAves() {
+		return Ave.listado.size();
+	}
+	
+	public String movimiento() {
+		return "volar";
+	}
+	
+	public static Ave crearHalcon(String nombre, int edad, String genero) {
+		Ave Ave = new Ave(nombre, edad, "montanas", genero, "cafe glorioso");
+		halcones++;
+		return Ave;
+	}
+	
+	public static Ave crearAguila(String nombre, int edad, String genero) {
+		Ave Ave = new Ave(nombre, edad, "montanas", genero, "blanco y amarillo");
+		aguilas++;
+		return Ave;
+	}
+
 }
